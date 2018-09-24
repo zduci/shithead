95b8426fbb03ab6e31fe6f05c37a1da2549a216d
Radu Busuioc <ra.busuioc@gmail.com>
2018-09-24 22:10:10 +0100

    Move App component to its own file

---
+++ app/javascript/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default App = props => (
+  <h1>Shithead</h1>
+)
--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-
-const App = props => (
-  <h1>Shithead</h1>
-)
+import App from '../App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App/>,
-    document.getElementById('root'),
+    document.getElementById('root')
   )
 })
