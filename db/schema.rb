05e026a5bfa1d957637ca6fc6621f0b14f0f5f5a
Radu Busuioc <ra.busuioc@gmail.com>
2018-09-24 20:29:50 +0100

    Create games table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_24_191709) do
+ActiveRecord::Schema.define(version: 2018_09_24_192756) do
+
+  create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "status"
+  end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
