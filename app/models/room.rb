38c51a4fdb83c8b4e13e9b64f9ac15a0cd36e11a
Radu Busuioc <ra.busuioc@gmail.com>
2018-09-26 17:06:49 +0100

    Create new game along with room

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,16 @@ class Room < ActiveRecord::Base
   friendly_id :name, use: :slugged
 
   has_many :games
+
+  before_create :ensure_game_exists
+
+  def game
+    games.last
+  end
+
+  private
+
+  def ensure_game_exists
+    self.games.new unless game
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -4,4 +4,9 @@ describe Room do
   context 'associations' do
     it { is_expected.to have_many(:games) }
   end
+
+  it 'creates a new game on create' do
+    room = Room.create!(name: 'kitchen')
+    expect(room.game).to be_present
+  end
 end
