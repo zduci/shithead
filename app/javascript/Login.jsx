034e2da7adc0e96cd4392229b57cba4953c4004f
Radu Busuioc <ra.busuioc@gmail.com>
2018-09-26 16:02:03 +0100

    Disable button unless name and room are filled

--- app/javascript/Login.jsx
+++ app/javascript/Login.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const LoginWrapper = styled.div`
+const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -18,7 +18,7 @@ const Input = styled.input`
   margin: 10px 0 10px 0;
 `
 
-const PlayButton = styled.div`
+const PlayButton = styled.button`
   display: inline-block;
   padding: 6px 12px;
   margin-bottom: 0;
@@ -33,25 +33,46 @@ const PlayButton = styled.div`
   border: 1px solid transparent;
   border-radius: 4px;
   border-color: black;
+  &.disabled {
+    color: grey;
+    border-color: grey;
+  }
 `
 
-export default function Login () {
-  return (
-    <LoginWrapper>
-      <h1>Shithead</h1>
-      <label htmlFor='name'>
-        Name:
-      </label>
-      <Input name='name'
-             type='text' />
-      <label htmlFor='room'>
-        Room:
-      </label>
-      <Input name='room'
-             type='text' />
-      <PlayButton>
-        Play
-      </PlayButton>
-    </LoginWrapper>
-  )
+export default class Login extends Component {
+  state = {
+    name: '',
+    room: ''
+  }
+
+  isButtonDisabled () {
+    return !(this.state.name.length && this.state.room.length)
+  }
+
+  setName = event => this.setState({ name: event.target.value })
+  setRoom = event => this.setState({ room: event.target.value })
+
+  render () {
+    return (
+      <LoginForm>
+        <h1>Shithead</h1>
+        <label htmlFor='name'>
+          Name:
+        </label>
+        <Input name='name'
+               type='text'
+               onChange={this.setName} />
+        <label htmlFor='room'>
+          Room:
+        </label>
+        <Input name='room'
+               type='text'
+               onChange={this.setRoom} />
+        <PlayButton disabled={!this.isButtonDisabled}
+                    className={this.isButtonDisabled() && 'disabled' } >
+          Play
+        </PlayButton>
+      </LoginForm>
+    )
+  }
 }
