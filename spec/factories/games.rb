d36722df7693ac4b9089490ffefcb586ce80bf76
Radu Busuioc <ra.busuioc@gmail.com>
2018-09-26 22:37:39 +0100

    Added factories for games and rooms for all game states

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :game do
     room
+
+    trait :joining do
+      status { Game::Status::JOINING }
+    end
+
+    trait :playing do
+      status { Game::Status::PLAYING }
+    end
+
+    trait :ended do
+      status { Game::Status::ENDED }
+    end
   end
 end
--- spec/factories/rooms.rb
+++ spec/factories/rooms.rb
@@ -1,5 +1,23 @@
 FactoryBot.define do
   factory :room do
     name { 'Shed' }
+
+    trait :joining do
+      after(:create) do |room|
+        create(:game, :joining, room: room)
+      end
+    end
+
+    trait :playing do
+      after(:create) do |room|
+        create(:game, :playing, room: room)
+      end
+    end
+
+    trait :ended do
+      after(:create) do |room|
+        create(:game, :ended, room: room)
+      end
+    end
   end
 end
