f425b30c92298aae75feac25b3334af801d3673d
Radu Busuioc <ra.busuioc@gmail.com>
2018-09-27 12:56:56 +0100

    Player names are unique to a game

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,5 @@
 class Player < ActiveRecord::Base
   belongs_to :game
+
+  validates_uniqueness_of :name, scope: :game_id
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,4 +4,8 @@ describe Player do
   context 'associations' do
     it { is_expected.to belong_to(:game) }
   end
+
+  context 'validations' do
+    it { should validate_uniqueness_of(:name).scoped_to(:game_id) }
+  end
 end
