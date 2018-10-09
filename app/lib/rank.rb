fced30d4778bcaba7806a7582a874076dd85b5cf
Radu Busuioc <ra.busuioc@gmail.com>
2018-10-09 20:50:40 +0100

    Rank has both name and value

--- app/lib/rank.rb
+++ app/lib/rank.rb
@@ -1,17 +1,24 @@
 class Rank
-  ACE = 'ace'.freeze
-  TWO = 'two'.freeze
-  THREE = 'three'.freeze
-  FOUR = 'four'.freeze
-  FIVE = 'five'.freeze
-  SIX = 'six'.freeze
-  SEVEN = 'seven'.freeze
-  EIGHT = 'eight'.freeze
-  NINE = 'nine'.freeze
-  TEN = 'ten'.freeze
-  JACK = 'jack'.freeze
-  QUEEN = 'queen'.freeze
-  KING = 'king'.freeze
+  attr_reader :name, :value
+
+  def initialize(name, value)
+    @name = name
+    @value = value
+  end
+
+  ACE = new('ace', 1)
+  TWO = new('two', 2)
+  THREE = new('three', 3)
+  FOUR = new('four', 4)
+  FIVE = new('five', 5)
+  SIX = new('six', 6)
+  SEVEN = new('seven', 7)
+  EIGHT = new('eight', 8)
+  NINE = new('nine', 9)
+  TEN = new('ten', 10)
+  JACK = new('jack', 11)
+  QUEEN = new('queen', 12)
+  KING = new('king', 13)
 
   ALL = [ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
          EIGHT, NINE, TEN, JACK, QUEEN, KING].freeze
