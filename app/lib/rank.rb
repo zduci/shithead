551e10cfea5da423d1c418e540ec2970a1007d42
Radu Busuioc <ra.busuioc@gmail.com>
2018-10-09 19:48:24 +0100

    Add Rank

---
+++ app/lib/rank.rb
@@ -0,0 +1,18 @@
+class Rank
+  ACE = 'ace'.freeze
+  TWO = 'two'.freeze
+  THREE = 'three'.freeze
+  FOUR = 'four'.freeze
+  FIVE = 'five'.freeze
+  SIX = 'six'.freeze
+  SEVEN = 'seven'.freeze
+  EIGHT = 'eight'.freeze
+  NINE = 'nine'.freeze
+  TEN = 'ten'.freeze
+  JACK = 'jack'.freeze
+  QUEEN = 'queen'.freeze
+  KING = 'king'.freeze
+
+  ALL = [ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
+         EIGHT, NINE, TEN, JACK, QUEEN, KING].freeze
+end
