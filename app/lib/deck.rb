a93463116e9f11e69623a49255d8fcf407c37cd1
Radu Busuioc <ra.busuioc@gmail.com>
2018-10-09 19:49:11 +0100

    Add basic Deck that can shuffle cards

---
+++ app/lib/deck.rb
@@ -0,0 +1,21 @@
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = build_deck
+  end
+
+  def shuffle
+    cards.shuffle!
+  end
+
+  private
+
+  def build_deck
+    Rank::ALL.flat_map do |rank|
+      Suit::ALL.flat_map do |suit|
+        Card.new(rank, suit)
+      end
+    end
+  end
+end
