ebdc407f08e1d819a6181139a83352a49464e6fd
Radu Busuioc <ra.busuioc@gmail.com>
2018-11-07 21:48:40 +0000

    Configure action cable for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://zduci-shithead.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://zduci-shithead.herokuapp.com', 'http://zduci-shithead.herokuapp.com']
 end
